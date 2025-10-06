cb57d605f2c415253a8ca9bfa7dc9fd9655e785f
Rohit Sahu <rohitrrrs1234@gmail.com>
2025-10-06 20:43:18 +0530

    Fix BarLoader import in header component for logout functionality

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+import { BarLoader } from "react-spinners";
 import {
   DropdownMenu,
   DropdownMenuContent,
